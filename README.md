88a7949eb5307e360bc57e7f57e1801402279ed8
bonedaddy <boned4ddy@protonmail.com>
2022-03-22 15:11:30 -0700

    add cairo installation script

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # shartnet_startkit
+
+# installation
+
+to prepare your environment for StarkNet development, Cairo and Python3.7 need to be installed. To do so a preparation script is provided, which is supported on Ubuntu systems, and likely with small modifications on mac osx systems.
+
+There are two installation options, one which installs the visual studio code cairo extension, and one which does not. The default behavior is to install this extension.
+
+```shell
+$> ./scripts/prepare.sh # installs visual studio code extension
+$> ./scripts/prepare.sh yes # installs visual studio code extension
+$> ./scripts/prepare.sh no # does not install visual studio code extension
+$> ./scripts/prepare.sh shart # does not install visual studio code extension
+```
+
+Then you will need to launch a new terminal window to read the newly set environment variables, and run the following command:
+
+```shell
+$> starknet deploy_account
+Sent deploy account contract transaction.
+
+NOTE: This is a modified version of the OpenZeppelin account contract. The signature is computed
+differently.
+
+Contract address: 0x07d733ba0cdde2c46e2840648f379b97efc767084cde3e2c508b05133afffaa0
+Public key: 0x066be9d2df098aee5b1d410148e3aeee6f0946d898a6aa68747d486f36479c7f
+Transaction hash: 0x6951f95eabdb2eac42bb3c09f507fb3b15eb65895486511856ce578db84a5bf
+```
+
+
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,11 +1,29 @@
 #! /bin/bash
 
+
+# starknet network supported: alpha-goerli
+
+# usage: prepare.sh <enable-vscode-ext>
+#
+#   enable-vscode-ext: installs vscode extension for cario (default: yes)
+
 # prepares an ubuntu environment for Cairo development
 # by installing the required dependencies, taking care 
 # to not override the system version of ubuntu.
 
-# this "should" be usable on mac osx, you will just need to uncomment
-# the `brew` install_dependencies function below
+# with a bit of modification this script will work on mac osx. you will need to
+# update teh `install_dependenceis` and `install_python37` functions first.
+
+# links
+# * https://www.cairo-lang.org/docs/quickstart.html
+# * https://www.cairo-lang.org/docs/hello_starknet/account_setup.html
+
+INSTALL_VSCODE_EXT="$1"
+
+if [[ "$INSTALL_VSCODE_EXT" == "" ]]; then
+    INSTALL_VSCODE_EXT="yes"
+fi
+
 
 function install_dependencies() {
     # if running macos comment the following line
@@ -25,6 +43,7 @@ function install_python37() {
     tar zxvf Python-3.7.12.tgz
     cd Python-3.7.12
     ./configure --enable-optimizations
+    # these steps below here will not work on mac osx
     sudo make -j$(nproc/2) altinstall
     echo "[INFO] it is recommended to add alternative python installations to system settings, if required (this may be damaging not 100% sure):"
     echo "ex: update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1"
@@ -58,6 +77,35 @@ cairo-run \
   cleanup
 }
 
+
+function install_vscode_ext() {
+    if [[ "$INSTALL_VSCODE_EXT" != "yes" ]]; then
+        return
+    fi
+    echo "[INFO] shartnet_startkit installing visual studio code extension"
+    wget https://github.com/starkware-libs/cairo-lang/releases/download/v0.8.0/cairo-0.8.0.vsix
+    code --install-extension cairo-0.8.0.vsix
+    rm cairo-0.8.0.vsix
+    echo """[INFO] it is recommended to enable the following settings on vscode
+
+
+\"editor.formatOnSave\": true,
+\"editor.formatOnSaveTimeout\": 1500
+"""
+}
+
+
+
+function setup_env_vars() {
+    if [[ "$SHELL" == "/bin/fish" ]]; then
+        echo "set -x STARKNET_NETWORK alpha-goerli" >> ~/.config/fish/config.fish
+        echo "set -x STARKNET_WALLET starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount" >> ~/.config/fish/config.fish
+    elif [[ "$SHELL" == "/bin/bash" ]]; then
+        echo "export STARKNET_NETWORK=alpha-goerli" >> ~/.bashrc
+        echo "export STARKNET_WALLET=starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount" >> ~/.bashrc
+    fi
+}
+
 echo "[INFO] shartnet_starkit installing dependencies"
 install_dependencies
 install_python37
@@ -69,3 +117,10 @@ install_cairo
 test_cairo_install
 
 echo "[INFO] shartnet_starkit installed cairo"
+install_vscode_ext
+
+echo "[INFO] shartnet_starkit configuring environment variables"
+setup_env_vars
+
+echo "[INFO] sharnet_startkit preparation complete. please run the following command from within a new shell to finalize"
+echo "starknet deploy_account"
